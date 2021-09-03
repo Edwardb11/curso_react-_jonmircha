05df8e8a9f7223515ddde16dd1b57f33ae90f1f7
Edward Brito <edwardbrito11@hotmail.com>
2021-09-03 10:33:32 -0400

    frameworks

--- framekors_css_react/src/App.js
+++ framekors_css_react/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Bootstrap from "./components/Bootstrap"
 function App() {
   return (
    <div>
      <h1>Frameworks CSS con React</h1>
+    <Bootstrap/>
    </div>
   );
 }
