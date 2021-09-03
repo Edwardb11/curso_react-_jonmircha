b5427f34a895e26df19ddf69e353ff177fd4eead
Edward Brito <edwardbrito11@hotmail.com>
2021-09-03 11:57:08 -0400

    Frameworks css

--- framekors_css_react/src/App.js
+++ framekors_css_react/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Bootstrap from "./components/Bootstrap"
-import Bulma from './components/Bulma';
+import MaterialUI from './components/MaterialUI';
+//import Bootstrap from "./components/Bootstrap"
+//import Bulma from './components/Bulma';
+// import Reactboostrap from "./components/Reactboostrap"
 function App() {
   return (
    <div>
      <h1>Frameworks CSS con React</h1>
     {/* <Bootstrap/> */}
-    <Bulma/>
+    {/* <Bulma/> */}
+    {/* <Reactboostrap></Reactboostrap> */}
+    <MaterialUI/>
    </div>
   );
 }
---
+++ framekors_css_react/src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MateriaCard from "./MateriaCard"
+import MateriaDrower from "./MateriaDrawer"
+
+export default function MaterialUI(){
+    return(
+        <>
+            <h2>MaterialUI</h2>
+            <MateriaCard/>
+            <MateriaDrower/>
+        </>
+    )
+}
