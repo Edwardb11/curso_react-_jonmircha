9e0a90b5be2baafab21d005aeecfe89040c004eb
Edward Brito <edwardbrito11@hotmail.com>
2021-09-03 10:51:03 -0400

    bulma y boostrap

--- framekors_css_react/src/App.js
+++ framekors_css_react/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Bootstrap from "./components/Bootstrap"
+import Bulma from './components/Bulma';
 function App() {
   return (
    <div>
      <h1>Frameworks CSS con React</h1>
-    <Bootstrap/>
+    {/* <Bootstrap/> */}
+    <Bulma/>
    </div>
   );
 }
